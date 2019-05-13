6e8bd6e7e02d35bee08bfd076f72238d3c4ab2f9
Craig Johnston <cjimti@gmail.com>
2019-05-13 16:33:52 -0700

    improved error message

--- es.go
+++ es.go
@@ -1,18 +1,20 @@
-/*
-   Copyright 2019 TXN2
+//
+//   Copyright 2019 TXN2
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+// Package es implements a simple Elasticsearch client.
 package es
 
 import (
@@ -144,7 +146,11 @@ func (es *Client) reqRes(url string, data []byte, method string) (int, Result, e
 
 	err = json.Unmarshal(res, &resObj)
 	if err != nil {
-		es.Log.Error("Error unmarshaling result object.", zap.Error(err))
+		es.Log.Error("Error unmarshaling result object.",
+			zap.ByteString("result", res),
+			zap.Error(err),
+		)
+		resObj.Error = string(res)
 		return 0, resObj, err
 	}
 
